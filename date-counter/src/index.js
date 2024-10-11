209faa46843d26bf43986778ef191758b1bcf42c
RajVardhanReddyKudumula <rajvardhan0730@gmail.com>
2024-10-11 17:18:12 +0530

    new file:   date-counter/.gitignore 	new file:   date-counter/README.md     new file:   date-counter/package-lock.json 	new file:   date-counter/package.json 	new file:   date-counter/public/favicon.ico 	new file:   date-counter/public/index.html 	new file:   date-counter/public/logo192.png 	new file:   date-counter/public/logo512.png 	new file:   date-counter/public/manifest.json 	new file:   date-counter/public/robots.txt 	new file:   date-counter/src/App.js 	new file:   date-counter/src/index.js

---
+++ date-counter/src/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>Step : {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count : {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `S{count} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
